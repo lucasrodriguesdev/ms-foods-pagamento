e7cb6ca9956ffdfc518e587488614ade1954b120
Lucas Rodrigues <lucas.rbit@gmail.com>
2024-01-27 11:08:09 -0300

    propriedades config

--- src/main/java/br/com/lrfoods/pagamentos/PagamentosApplication.java
+++ src/main/java/br/com/lrfoods/pagamentos/PagamentosApplication.java
@@ -2,7 +2,6 @@ package br.com.lrfoods.pagamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class PagamentosApplication {
 
--- src/main/java/br/com/lrfoods/pagamentos/config/Configuracao.java
+++ src/main/java/br/com/lrfoods/pagamentos/config/Configuracao.java
@@ -1,2 +1,14 @@
-package br.com.lrfoods.pagamentos.config;public class Configuracao {
+package br.com.lrfoods.pagamentos.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Configuracao {
+
+    @Bean
+    public ModelMapper obterModelMapper(){
+        return new ModelMapper();
+    }
 }
